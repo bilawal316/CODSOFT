7cc71b706eb83de67821a56064ea7e0c5a2f68aa
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-09-18 16:33:03 +0000

    Updated skills2.js

--- app/components/skill/skill2.js
+++ app/components/skill/skill2.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Image from 'next/image';
 
 const Skill2 = () => {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.div initial={{ x: -200, opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1, x: 0 }}>
         <div className="relative">
-          <img
-            src="./next.png"
+          <Image
+            src="/next.png"
+            width={32}
+            height={32} 
             className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
           />
           <div className="absolute top-0 left-0 opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-24 h-24 xl:w-32 xl:h-32 rounded-full z-10">
