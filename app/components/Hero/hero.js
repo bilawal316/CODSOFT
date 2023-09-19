3f936a3625f42abb19c23213877940a957ed60e7
bilawal316 <mbilawalzaman@gmail.com>
2023-09-19 16:01:51 +0500

    Updated hero component and page

--- app/components/Hero/hero.js
+++ app/components/Hero/hero.js
@@ -1,32 +1,52 @@
-import Skills from "./components/Skills/skills";
-import Projects from "./components/Projects/projects";
-import Contact from "./components/contact/contact";
+"use client";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Link from "next/link";
 import Image from "next/image";
+import BackgroundCircles from "../../components/backgroundCircles/backgroundCircles";
 
-const Page = () => {
+const Hero = () => {
+  const [text, count] = useTypewriter({
+    words: ["Hi My Name is M Bilawal Zaman", "Designer", "Web Developer", "Coder"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-500 scrollbar-thumb-[#f7AB0A]/80 z-0">
-      <title>Bilawals Portfolio</title>
-      <Header />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center ">
-            <Image
-              className="rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="/logo.png"
-              alt=""
-              width={10}
-              height={10}
-            />
-          </div>
-        </footer>
-      </Link>
+    <div className="h-screen flex flex-col space-y-6 items-center justify-center text-center overflow-hidden">
+      <div className="z-0 pt-48">
+        <BackgroundCircles />
+      </div>
+      <Image
+        className="relative rounded-full mx-auto object-cover"
+        src="/bilawal.jpeg"
+        alt=""
+        width={120}
+        height={120}
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Web Developer</h2>
+      </div>
+      <h1 className="text-5xl lg:text-6xl font-semibold px-10 ">
+        <span>{text}</span>
+        <Cursor cursorColor="#F7ABBA" />
+      </h1>
+      <div className="pt-5 z-50">
+        <Link href="#about">
+          <button className="heroButton">About</button>
+        </Link>
+        <Link href="#experience">
+          <button className="heroButton">Experience</button>
+        </Link>
+        <Link href="#skills">
+          <button className="heroButton">Skills</button>
+        </Link>
+        <Link href="#projects">
+          <button className="heroButton">Projects</button>
+        </Link>
+      </div>
     </div>
   );
 };
 
-export default Page;
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,18 @@
-import React from "react";
-import Header from "./components/header/header";
-import Hero from "./components/Hero/hero";
-import About from "./components/About/about";
-import Experience from "./components/Experience/experience";
 import Skills from "./components/Skills/skills";
 import Projects from "./components/Projects/projects";
 import Contact from "./components/contact/contact";
-<<<<<<< HEAD
-=======
 import Link from "next/link";
 import Image from "next/image";
->>>>>>> 0750b0c6e8c2061fe88b56e5cc33acb3fab27a1b
+import Header from "./components/header/header"; // Make sure to import Header
 
 const Page = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-500 scrollbar-thumb-[#f7AB0A]/80 z-0">
-      <title>Bilawal&apos;s Portfolio</title>
-
+      <title>Bilawals Portfolio</title>
       <Header />
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
-      <section id="about" className="snap-center">
-        <About />
-      </section>
-      <section id="experience" className="snap-center">
-        <Experience />
-      </section>
-      <section id="skills" className="snap-start">
-        <Skills />
-      </section>
-      <section id="projects" className="snap-start">
-        <Projects />
-      </section>
-      <section id="contact" className="snap-start">
-        <Contact />
-      </section>
-<<<<<<< HEAD
-=======
+      <Skills />
+      <Projects />
+      <Contact />
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center ">
@@ -51,7 +26,6 @@ const Page = () => {
           </div>
         </footer>
       </Link>
->>>>>>> 0750b0c6e8c2061fe88b56e5cc33acb3fab27a1b
     </div>
   );
 };
