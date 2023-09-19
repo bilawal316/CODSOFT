b76d29f89d167de99214fbb95b9401a5294a1f93
bilawal316 <mbilawalzaman@gmail.com>
2023-09-19 16:27:57 +0500

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,36 @@
 import Skills from "./components/Skills/skills";
 import Projects from "./components/Projects/projects";
 import Contact from "./components/contact/contact";
-import Link from "next/link";
-import Image from "next/image";
-import Header from "./components/header/header"; // Make sure to import Header
+import Hero from "./components/Hero/hero";
+import About from "./components/About/about";
+import Experience from "./components/Experience/experience";
+import Header from "./components/header/header";
+import Footer from "./components/Footer/footer";
 
 const Page = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-500 scrollbar-thumb-[#f7AB0A]/80 z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-track-gray-500 scrollbar-thumb-[#f7AB0A]/80 z-0">
       <title>Bilawals Portfolio</title>
       <Header />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center ">
-            <Image
-              className="rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="/logo.png"
-              alt=""
-              width={10}
-              height={10}
-            />
-          </div>
-        </footer>
-      </Link>
+      <section id="hero">
+        <Hero />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="experience">
+        <Experience />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
     </div>
   );
 };
