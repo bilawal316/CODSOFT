1d9fe1302a0ae7d97f522f1359f567c54afdb1b7
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-09-21 10:37:32 +0000

    add app/components/About/about.js, app/components/Skills/skills.js, app/page.tsx, next.config.js, package.json

---
+++ app/components/Footer/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="sticky bottom-5 w-full cursor-pointer">
+      <Link href="#hero">
+        <div className="flex items-center justify-center">
+          <Image
+            className="rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            src="/logo.png"
+            alt=""
+            width={50}
+            height={50}
+          />
+        </div>
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
